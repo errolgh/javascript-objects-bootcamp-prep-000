5612479bbfbe5e1407d2f294e23ec89b6dc25a64
Errol W <ewatsonrei@gmail.com>
2019-01-03 23:04:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -12,7 +12,3 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
-
-// "Slowdive" By Alison
-
-/* Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist. */
